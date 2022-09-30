730ffca120b9d5763c94395aa3c5239a54d0c0a7
Xandre <xandredepaula2312@gmail.com>
2022-09-30 03:50:56 -0300

    Passando as informações para view, maneira limpa

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -7,7 +7,16 @@ class HomeController
         //verificando se tem algum registro na tabela
         try {
             $colecaoFilmes = Filmes::selecionaTodos();
-            var_dump($colecaoFilmes);
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $templete = $twig->load('home.html');
+
+
+            $parametros = array();
+            $parametros['filmes'] = $colecaoFilmes;
+            $conteudo =  $templete->render($parametros);
+
+            echo $conteudo;
         } catch (Exception $e) {
             echo $e->getMessage();
         }
